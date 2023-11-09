f8cbfbf7929af3957b743b34699e521558a686dc
Seggenz <filipwesolowski09@gmail.com>
2023-11-09 20:50:55 +0100

    first commit

--- task/main.py
+++ task/main.py
@@ -1,7 +1,44 @@
+from task.rectangle import Rectangle
+
+
 def main():
     # TODO: 1. Prepare example Rectangle data
+    rect1 = Rectangle(0, 0, 10, 10)
+    rect2 = Rectangle(5, 5, 15, 15)  # intersect with rect1
+    rect3 = Rectangle(10, 10, 20, 20)  # shares edges with rect1, but no intersection
+    rect4 = Rectangle(1, 1, 9, 9)  # inside rect1
+    rect5 = Rectangle(11, 11, 21, 21)  # outside rect1, no intersection
+
     # TODO: 2. Verify implemented methods
-    pass
+    print("Is rect1 contains rect2?")
+    print(rect1.contains(rect2))
+
+    print("Is rect2 contains rect1?")
+    print(rect2.contains(rect1))
+
+    print("Is rect1 contains rect3?")
+    print(rect1.contains(rect3))
+
+    print("Is rect1 contains rect4?")
+    print(rect1.contains(rect4))
+
+    print("Is rect1 contains rect5?")
+    print(rect1.contains(rect5))
+
+    print("Is rect1 intersect with rect2?")
+    print(rect1.intersect(rect2))
+
+    print("Is rect2 intersect with rect1?")
+    print(rect2.intersect(rect1))
+
+    print("Is rect1 intersect with rect3?")
+    print(rect1.intersect(rect3))
+
+    print("Is rect1 intersect with rect4?")
+    print(rect1.intersect(rect4))
+
+    print("Is rect1 intersect with rect5?")
+    print(rect1.intersect(rect5))
 
 
 if __name__ == "__main__":
--- task/rectangle.py
+++ task/rectangle.py
@@ -2,13 +2,18 @@ from dataclasses import dataclass
 
 
 @dataclass
-class Rectangle:
-    # TODO: Propose data format to hold rectangle information
+class Rectangle:  # I assumed that this is a 2D space
+    x0: int  # X-coordinate of the top-Left corner
+    y0: int  # Y-coordinate of the top-left corner
+    x1: int  # X-coordinate of the bottom-right corner
+    y1: int  # Y-coordinate of the bottom-right corner
 
     def contains(self, other: "Rectangle") -> bool:
-        # TODO: Implement
-        raise NotImplementedError
+        return (self.x0 <= other.x0 <= self.x1 and
+                self.y0 <= other.y0 <= self.y1 and
+                self.x0 <= other.x1 <= self.x1 and
+                self.y0 <= other.y1 <= self.y1)
 
     def intersect(self, other: "Rectangle") -> bool:
-        # TODO: Implement
-        raise NotImplementedError
+        return (self.x0 < other.x1 and self.x1 > other.x0 and
+                self.y0 < other.y1 and self.y1 > other.y0)
